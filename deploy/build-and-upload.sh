95be83f7eaf644f29c64913090e5c74ffa01f9c1
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 02:16:53 -0700

    upload artifact to s3

--- deploy/build-and-upload.sh
+++ deploy/build-and-upload.sh
@@ -2,11 +2,13 @@
 set -e
 
 APP_NAME="pathfinder-character-tool"
+SW_RELEASE_BUCKET="jhg-sw-releases"
 PROJECT_ROOT_DIR="${0%/*}/../"
 SOURCE_DIR="${PROJECT_ROOT_DIR}app"
 APP_VERSION=$(node -p -e "require('${SOURCE_DIR}/package.json').version")
-ARTIFACT_PATH="${PROJECT_ROOT_DIR}${APP_NAME}-${APP_VERSION}.tar.gz"
-echo $ARTIFACT_PATH
+ARTIFACT_FILENAME="${APP_NAME}-${APP_VERSION}.tar.gz"
+ARTIFACT_PATH="${PROJECT_ROOT_DIR}${ARTIFACT_FILENAME}"
+APP_S3_PATH="s3://${SW_RELEASE_BUCKET}/${APP_NAME}/${ARTIFACT_FILENAME}"
 
 if [ ! -e "${PROJECT_ROOT_DIR}deploy/build-and-upload.sh" ]; then
   echo "Please run this script from the root of the git repo in which this script lives."
@@ -14,19 +16,19 @@ if [ ! -e "${PROJECT_ROOT_DIR}deploy/build-and-upload.sh" ]; then
 fi
 
 
-
-
 pushd $PROJECT_ROOT_DIR
-
-  
-
   echo "Building App"
   if [ -e "${ARTIFACT_PATH}" ]; then
     rm ${ARTIFACT_PATH}
   fi
 
-  tar -czvf ${ARTIFACT_PATH} ${SOURCE_DIR}
+  tar -czf ${ARTIFACT_PATH} ${SOURCE_DIR} > /dev/null
 
-  aws s3
+  ARTIFACT_EXISTS_AWS=$(aws s3 ls ${APP_S3_PATH})
+  if [ ! -z "${ARTIFACT_EXISTS_AWS}" ]; then
+    echo "This file exists in s3 already. Cancel this script with ^C within 10s or it will be overwritten."
+    sleep 10
+  fi
+  aws s3 cp ${ARTIFACT_PATH} ${APP_S3_PATH}
 
 popd
