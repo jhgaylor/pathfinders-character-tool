fa117579ca887c37ed3a863fc2f611b835b832dc
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 03:13:29 -0700

    output useful information for connecting to the new instance

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -5,3 +5,7 @@ output "pathfinder-character-tool-public-dns" {
 output "pathfinder-character-tool-key-name" {
   value = "${aws_instance.pathfinder-character-tool.key_name}"
 }
+
+output "pathfinder-character-tool-ssh-example" {
+  value = "ssh ubuntu@${aws_instance.pathfinder-character-tool.public_dns} -i ~/.ssh/${aws_instance.pathfinder-character-tool.key_name}"
+}
