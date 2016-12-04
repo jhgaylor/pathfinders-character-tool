64d35f8930d6973587c8f51a613ea7b7ff698e47
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 02:44:04 -0700

    terraform fmt

--- deploy/terraform/instance.tf
+++ deploy/terraform/instance.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "pathfinder-character-tool" {
   ami      = "ami-34913254"
   key_name = "jake-laptop-aws"
 
-  # iam_instance_profile = "${aws_iam_instance_profile.pathfinder-character-tool.name}"
-  instance_type = "t2.small"
-  user_data     = "${file("../bootstrap-instance.sh")}"
+  iam_instance_profile = "${aws_iam_instance_profile.pathfinder-character-tool.name}"
+  instance_type        = "t2.small"
+  user_data            = "${file("../bootstrap-instance.sh")}"
 
   tags {
     Name = "${data.terraform_remote_state.dev_vpc.vpc_name}-pathfinder-character-tool"
