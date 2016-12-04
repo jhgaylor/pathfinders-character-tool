33198260af8a1b33723d3be0c3b584332f69a6f1
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 02:01:07 -0700

    ensure the app is run from the correct place

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+set -e
 
-echo "Building App"
+
+PROJECT_ROOT_DIR="${0%/*}/../"
+SOURCE_DIR="${PROJECT_ROOT_DIR}app"
+echo $SOURCE_DIR
+if [ ! -e "${PROJECT_ROOT_DIR}deploy/build.sh" ]; then
+  echo "Please run this script from the root of the git repo in which this script lives."
+  exit 1
+fi
+
+pushd $PROJECT_ROOT_DIR
+  echo "Building App"
+  tar -czvf ${PROJECT_ROOT_DIR}/app.tar.gz ${PROJECT_ROOT_DIR}/app
+popd
