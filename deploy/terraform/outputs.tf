90bcd01b5ed1c58e7a59285672e4fb9f74967b90
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 04:04:41 -0700

    add output for app url

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -5,3 +5,7 @@ output "pathfinder-character-tool-public-dns" {
 output "pathfinder-character-tool-ssh-example" {
   value = "ssh ubuntu@${aws_instance.pathfinder-character-tool.public_dns} -i ~/.ssh/${aws_instance.pathfinder-character-tool.key_name}.pem"
 }
+
+output "pathfinder-character-tool-app-url" {
+  value = "${aws_instance.pathfinder-character-tool.public_dns}:3000"
+}
