f4495f4ba6f94256d29859cee5808c59270549ac
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 04:50:48 -0700

    turn it off for the night

--- deploy/terraform/instance.tf
+++ deploy/terraform/instance.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "pathfinder-character-tool" {
-  ami      = "ami-34913254"
-  key_name = "jake-laptop-aws"
-
-  iam_instance_profile = "${aws_iam_instance_profile.pathfinder-character-tool.name}"
-  instance_type        = "t2.small"
-  subnet_id            = "${element(data.terraform_remote_state.dev_vpc.public_subnet_ids, 1)}"
-  user_data            = "${file("../bootstrap-instance.sh")}"
-  vpc_security_group_ids    = ["${data.terraform_remote_state.dev_vpc.base_security_group_id}", "${aws_security_group.pathfinder-character-tool.id}"]
+  ami                    = "ami-34913254"
+  key_name               = "jake-laptop-aws"
+  count                  = 0
+  iam_instance_profile   = "${aws_iam_instance_profile.pathfinder-character-tool.name}"
+  instance_type          = "t2.small"
+  subnet_id              = "${element(data.terraform_remote_state.dev_vpc.public_subnet_ids, 1)}"
+  user_data              = "${file("../bootstrap-instance.sh")}"
+  vpc_security_group_ids = ["${data.terraform_remote_state.dev_vpc.base_security_group_id}", "${aws_security_group.pathfinder-character-tool.id}"]
 
   tags {
     Name = "${lower(data.terraform_remote_state.dev_vpc.vpc_name)}-pathfinder-character-tool"
