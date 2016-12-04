89af30548b48c369a223a0daae0434c93573a0ee
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 03:13:43 -0700

    build in a private subnet

--- deploy/terraform/instance.tf
+++ deploy/terraform/instance.tf
@@ -4,10 +4,11 @@ resource "aws_instance" "pathfinder-character-tool" {
 
   iam_instance_profile = "${aws_iam_instance_profile.pathfinder-character-tool.name}"
   instance_type        = "t2.small"
+  subnet_id            = "${element(data.terraform_remote_state.dev_vpc.private_subnet_ids, 1)}"
   user_data            = "${file("../bootstrap-instance.sh")}"
 
   tags {
-    Name = "${data.terraform_remote_state.dev_vpc.vpc_name}-pathfinder-character-tool"
+    Name = "${lower(data.terraform_remote_state.dev_vpc.vpc_name)}-pathfinder-character-tool"
   }
 }
 
