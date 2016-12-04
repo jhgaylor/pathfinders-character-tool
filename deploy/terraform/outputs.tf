f29161c7731ba403c142651e644b75f1171e5c71
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 03:28:53 -0700

    cleanup outputs

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -1,15 +1,7 @@
-output "pathfinder-character-tool-public-ip" {
-  value = "${aws_instance.pathfinder-character-tool.public_ip}"
-}
-
 output "pathfinder-character-tool-public-dns" {
   value = "${aws_instance.pathfinder-character-tool.public_dns}"
 }
 
-output "pathfinder-character-tool-key-name" {
-  value = "${aws_instance.pathfinder-character-tool.key_name}"
-}
-
 output "pathfinder-character-tool-ssh-example" {
-  value = "ssh ubuntu@${aws_instance.pathfinder-character-tool.public_ip} -i ~/.ssh/${aws_instance.pathfinder-character-tool.key_name}.pem"
+  value = "ssh ubuntu@${aws_instance.pathfinder-character-tool.public_dns} -i ~/.ssh/${aws_instance.pathfinder-character-tool.key_name}.pem"
 }
