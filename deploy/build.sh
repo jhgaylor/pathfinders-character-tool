d0900d6df9dc1fb9a29990630f84a9e37e83c3ef
Jake Gaylor <jgaylor@fsw.com>
2016-12-04 02:06:34 -0700

    add variables

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 set -e
 
-
+APP_NAME="pathfinder-character-tool"
 PROJECT_ROOT_DIR="${0%/*}/../"
 SOURCE_DIR="${PROJECT_ROOT_DIR}app"
-echo $SOURCE_DIR
+APP_VERSION=$(node -p -e "require('${SOURCE_DIR}/package.json').version")
+ARTIFACT_PATH="${PROJECT_ROOT_DIR}${APP_NAME}-${APP_VERSION}.tar.gz"
+echo $ARTIFACT_PATH
+
 if [ ! -e "${PROJECT_ROOT_DIR}deploy/build.sh" ]; then
   echo "Please run this script from the root of the git repo in which this script lives."
   exit 1
 fi
 
+
 pushd $PROJECT_ROOT_DIR
+
+  
+
   echo "Building App"
-  tar -czvf ${PROJECT_ROOT_DIR}/app.tar.gz ${PROJECT_ROOT_DIR}/app
+  if [ -e "${ARTIFACT_PATH}" ]; then
+    rm ${ARTIFACT_PATH}
+  fi
+
+  tar -czvf ${ARTIFACT_PATH} ${SOURCE_DIR}
 popd
