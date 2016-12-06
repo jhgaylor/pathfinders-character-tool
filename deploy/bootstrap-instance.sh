932ee50a6009da65249e8d2ad5a10bfc41c7faae
Jake Gaylor <jgaylor@fsw.com>
2016-12-06 00:47:41 -0700

    remove dead code

--- deploy/bootstrap-instance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# ubuntu 14's ssl something or another needs upgrading to work with the ansible provisioner
-# http://stackoverflow.com/questions/29134512/insecureplatformwarning-a-true-sslcontext-object-is-not-available-this-prevent
-apt-get install libffi-dev libssl-dev python-dev build-essential -y
