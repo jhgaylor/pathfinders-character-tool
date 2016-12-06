ecf9cf1113ff0b785b8f3feff35bbccf59059380
Jake Gaylor <jgaylor@fsw.com>
2016-12-06 00:48:37 -0700

    remove code that wasn't needed due to our base ami

--- deploy/packer/scripts/setup.sh
+++ deploy/packer/scripts/setup.sh
@@ -4,8 +4,6 @@
 apt-get install libffi-dev libssl-dev python-dev python-pip build-essential -y
 
 # Install Ansible repository.
-apt-get -y update && apt-get -y upgrade
-apt-get -y install software-properties-common
 apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
