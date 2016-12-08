a5f45c81a65fe89db5563a607abeeecfd412cd41
Jake Gaylor <jgaylor@fsw.com>
2016-12-07 22:49:15 -0700

    re-emit outputs of module

---
+++ deploy/terraform/output.tf
@@ -0,0 +1,11 @@
+output "app-public-dns" {
+  value = "${module.pathfinder-character-tool.app-public-dns}"
+}
+
+output "app-ssh-example" {
+  value = "${module.pathfinder-character-tool.app-ssh-example}"
+}
+
+output "app-app-url" {
+  value = "${module.pathfinder-character-tool.app-app-url}"
+}
