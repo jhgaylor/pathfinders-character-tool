15a34364cda2a855c4f4b55f071427d7b7c4774a
Jake Gaylor <jgaylor@fsw.com>
2016-12-08 01:35:22 -0700

    have packer pass in UI and API versiosn

--- deploy/packer/build-ami.sh
+++ deploy/packer/build-ami.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-APP_VERSION=$(node -p -e "require('../../app/package.json').version")
-packer build -var "app_version=${APP_VERSION}" app.json
+API_VERSION=$(node -p -e "require('../../api-app/package.json').version")
+UI_VERSION=$(node -p -e "require('../../ui-app/package.json').version")
+packer build -var "api_version=${API_VERSION}" -var "ui_version=${UI_VERSION}" app.json
