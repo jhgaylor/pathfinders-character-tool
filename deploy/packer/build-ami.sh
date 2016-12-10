d8699f533ad8bca01d0bcfff042d0d228bddb416
Jake Gaylor <jgaylor@fsw.com>
2016-12-10 16:36:52 -0700

    rework packer for chef

--- deploy/packer/build-ami.sh
+++ deploy/packer/build-ami.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-API_VERSION=$(node -p -e "require('../../api-app/package.json').version")
-UI_VERSION=$(node -p -e "require('../../ui-app/package.json').version")
-packer build -var "api_version=${API_VERSION}" -var "ui_version=${UI_VERSION}" app.json
+# API_VERSION=$(node -p -e "require('../../api-app/package.json').version")
+# UI_VERSION=$(node -p -e "require('../../ui-app/package.json').version")
+
+# Grab cookbooks from berksfiles - this is solo silliness
+# The 'chef exec' assumes that you've used the chefdk
+pushd ../chef/cookbooks
+  for dir in $(ls); do
+    chef exec berks vendor --berksfile ${dir}/Berksfile ../vendor/cookbooks
+  done
+popd
+
+# packer build -var "api_version=${API_VERSION}" -var "ui_version=${UI_VERSION}" app.json
+packer build app.json
